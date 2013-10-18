e08ecc48d43705b938f749be37f36fa6686bfff3
Michał Pasternak <michal.dtz@gmail.com>
2013-10-18 08:48:00 +0200

    The test project. Login into admin as test/test

---
+++ test_project/test_project/urls.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.conf.urls.static import static
+from django.contrib import admin
+from test_app.views import TestView
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', TestView.as_view(), name='home'),
+    # url(r'^test_project/', include('test_project.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
