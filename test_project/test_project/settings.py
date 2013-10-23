86d7332825bf6fc1261333345744bfa5af8cf7a1
Pablo Martín <goinnn@gmail.com>
2013-10-23 19:29:59 +0200

    Now works with django 1.3

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -147,15 +147,9 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
-            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
